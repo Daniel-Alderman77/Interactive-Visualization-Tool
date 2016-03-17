9e57dd1a33ab9303241a72bd86031118c9562b40
Daniel Alderman <daniel.alderman77@gmail.com>
2016-03-17 13:58:40 +0000

    Modularise each subplot into separate functions

--- Startup.py
+++ Startup.py
@@ -8,34 +8,53 @@ totalMemory = int(memoryData[0])
 task1 = int(memoryData[1])
 task2 = int(memoryData[2])
 
-def Render():
-    # plot with various axes scales
-    plt.figure(1)
-
+def MemoryAllocation(currentValue):
     # Memory Allocation
     plt.subplot(221)
-    plt.plot([task1 + task2, 700, 600, 500, 800])
+    plt.plot(currentValue)
     plt.axis([0, 6, 0, 1000])
     plt.ylabel('Memory Allocation')
     plt.xlabel('Time')
     plt.title('Memory Allocation Over Time')
 
+def Task1(currentValue):
     # Task 1
     plt.subplot(222)
-    plt.plot(totalMemory - (task1 + task2))
+    plt.plot(currentValue)
     plt.axis([0, 6, 0, 1000])
     plt.ylabel('Memory Allocation')
     plt.xlabel('Time')
     plt.title('Task 1 Memory Usage')
 
+def Task2(currentValue):
     # Task 2
     plt.subplot(223)
-    plt.plot(totalMemory - (task1 + task2))
-    plt.axis([0, 6, 0, 1000])
+    plt.plot(currentValue)
+    plt.axis()
     plt.ylabel('Memory Allocation')
     plt.xlabel('Time')
     plt.title('Task 2 Memory Usage')
 
+def Render():
+    # plot with various axes scales
+    plt.figure(1)
+
+    task1Usage = [task1, 400, 350, 500, 400]
+    task2Usage = [task2, 350, 400, 500, 600]
+
+    plt.ion()
     plt.show()
+
+    for i in range(100):
+
+
+        memoryAllocation = task1Usage + task2Usage
+
+        MemoryAllocation(memoryAllocation)
+        Task1(task1Usage)
+        Task2(task2Usage)
+
+        plt.draw()
+        plt.pause(0.05)
 
 Render()
