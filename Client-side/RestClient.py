19067a2bf9c2bc5bc75648ec91a5defba9103690
Daniel Alderman <daniel.alderman77@gmail.com>
2016-03-20 13:43:35 +0000

    GetRequest method now raises ConnectTimeout exception

--- Client-side/RestClient.py
+++ Client-side/RestClient.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import ConnectTimeout
 
 class RestClient:
 
@@ -6,10 +7,17 @@ class RestClient:
         self.name = self
 
     def GetRequest(self):
-        session = requests.Session()
 
-        request = session.get('http://github.com', timeout = 0.05)
+        request = None
 
-        print(request)
+        try:
+            session = requests.Session()
+
+            request = session.get('http://github.com', timeout = 0.05)
+
+            print(request)
+
+        except ConnectTimeout:
+            print "Connection has timed out"
 
         return request
