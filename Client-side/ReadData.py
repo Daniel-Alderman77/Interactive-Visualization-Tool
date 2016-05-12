77a7c71104a69d14cf8913b5f28be1f760670341
Daniel Alderman <daniel.alderman77@gmail.com>
2016-05-12 13:53:21 +0100

    ReadData.py has been amended to follow PEP8

--- Client-side/ReadData.py
+++ Client-side/ReadData.py
@@ -1,39 +1,40 @@
 from lxml import etree
 
+
 class ReadData:
 
     def __init__(self):
         self.name = self
 
-    def ParseXML(self, filename):
-        totalMemory = None
+    def parse_xml(self, filename):
+        total_memory = None
         task1 = None
         task2 = None
 
         root = etree.parse(filename)
 
-        logNode = root.find('.//LOG-NODE')
-        logNodeContents = logNode.getchildren()
-        for content in logNodeContents:
+        log_node = root.find('.//LOG-NODE')
+        log_node_contents = log_node.getchildren()
+        for content in log_node_contents:
             if content.get('Name') == 'Memory':
-                totalMemory = content.get('Value')
+                total_memory = content.get('Value')
 
-        print("Total Memory: %s" % totalMemory)
+        print("Total Memory: %s" % total_memory)
 
         actions = root.findall('.//LOG-ACTION')
 
-        logActionContents = actions[0].getchildren()
-        for content in logActionContents:
+        log_action_contents = actions[0].getchildren()
+        for content in log_action_contents:
             if content.get('Name') == 'Memory_Allocated':
                 task1 = content.get('Value')
 
-        print("Task 1 Memory: %s" %task1)
+        print("Task 1 Memory: %s" % task1)
 
-        logActionContents = actions[1].getchildren()
-        for content in logActionContents:
+        log_action_contents = actions[1].getchildren()
+        for content in log_action_contents:
             if content.get('Name') == 'Memory_Allocated':
                 task2 = content.get('Value')
 
-        print("Task 2 Memory: %s" %task2)
+        print("Task 2 Memory: %s" % task2)
 
-        return [totalMemory, task1, task2]
+        return [total_memory, task1, task2]
