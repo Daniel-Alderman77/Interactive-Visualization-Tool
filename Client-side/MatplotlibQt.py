b891d1502d540a5e470aaa48c35d3dc3f4acd2bb
Daniel Alderman <daniel.alderman77@gmail.com>
2016-05-12 13:36:22 +0100

    MatplotlibQt.py has been amended to follow PEP8

--- Client-side/MatplotlibQt.py
+++ Client-side/MatplotlibQt.py
@@ -1,22 +1,24 @@
 # Taken from http://scipy-cookbook.readthedocs.org/items/Matplotlib_PySide.html
 
 import sys
-import matplotlib
-matplotlib.use('Qt4Agg')
-matplotlib.rcParams['backend.qt4']='PySide'
 
+import matplotlib
+from PySide import QtGui
+
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4'] = 'PySide'
+
+# Warnings need to be ignored to preserve import order
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-
-from PySide import QtGui
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
 
     # generate the plot
-    fig = Figure(figsize=(600,600), dpi=72, facecolor=(1,1,1), edgecolor=(0,0,0))
+    fig = Figure(figsize=(600, 600), dpi=72, facecolor=(1, 1, 1), edgecolor=(0, 0, 0))
     ax = fig.add_subplot(111)
-    ax.plot([0,1])
+    ax.plot([0, 1])
     # generate the canvas to display the plot
     canvas = FigureCanvas(fig)
 
