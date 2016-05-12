ce58e07b35bc0dedc2ac34cdbf83effcb0435bc4
Daniel Alderman <daniel.alderman77@gmail.com>
2016-05-12 13:48:06 +0100

    GaugePlottingTest.py has been amended to follow PEP8

--- Client-side/GaugePlottingTest.py
+++ Client-side/GaugePlottingTest.py
@@ -1,44 +1,47 @@
 # Taken from http://nbviewer.jupyter.org/gist/nicolasfauchereau/794df533eca594565ab3
 
-import sys
-import matplotlib
-matplotlib.use('Qt4Agg')
-matplotlib.rcParams['backend.qt4']='PySide'
-
-from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+import numpy as np
 
 from PySide import QtGui
 
+import sys
+import matplotlib
+
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4'] = 'PySide'
+
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+
 from matplotlib import cm
 from matplotlib import pyplot as plt
-import numpy as np
 
 from matplotlib.patches import Circle, Wedge, Rectangle
 
+
 def degree_range(n):
-    start = np.linspace(0,180,n+1, endpoint=True)[0:-1]
-    end = np.linspace(0,180,n+1, endpoint=True)[1::]
-    mid_points = start + ((end-start)/2.)
+    start = np.linspace(0, 180, n + 1, endpoint=True)[0:-1]
+    end = np.linspace(0, 180, n + 1, endpoint=True)[1::]
+    mid_points = start + ((end - start) / 2.)
     return np.c_[start, end], mid_points
+
 
 def rot_text(ang):
     rotation = np.degrees(np.radians(ang) * np.pi / np.pi - np.radians(90))
     return rotation
 
-def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', cat=1, title=''):
-    app = QtGui.QApplication(sys.argv)
 
+def gauge(labels=['LOW', 'MEDIUM', 'HIGH', 'VERY HIGH', 'EXTREME'], colors='jet_r', cat=1, title=''):
+    app = QtGui.QApplication(sys.argv)
 
     """
     some sanity checks first
 
     """
 
-    N = len(labels)
+    n = len(labels)
 
-    if cat > N:
-        raise Exception("\n\nThe category ({}) is greated than the length\nof the labels ({})".format(cat, N))
-
+    if cat > n:
+        raise Exception("\n\nThe category ({}) is greated than the length\nof the labels ({})".format(cat, n))
 
     """
     if colors is a string, we assume it's a matplotlib colormap
@@ -46,14 +49,14 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
     """
 
     if isinstance(colors, str):
-        cmap = cm.get_cmap(colors, N)
-        cmap = cmap(np.arange(N))
-        colors = cmap[::-1,:].tolist()
+        cmap = cm.get_cmap(colors, n)
+        cmap = cmap(np.arange(n))
+        colors = cmap[::-1, :].tolist()
     if isinstance(colors, list):
-        if len(colors) == N:
+        if len(colors) == n:
             colors = colors[::-1]
         else:
-            raise Exception("\n\nnumber of colors {} not equal to number of categories{}\n".format(len(colors), N))
+            raise Exception("\n\nnumber of colors {} not equal to number of categories{}\n".format(len(colors), n))
 
     """
     begins the plotting
@@ -61,7 +64,7 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
 
     fig, ax = plt.subplots()
 
-    ang_range, mid_points = degree_range(N)
+    ang_range, mid_points = degree_range(n)
 
     labels = labels[::-1]
 
@@ -71,9 +74,9 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
     patches = []
     for ang, c in zip(ang_range, colors):
         # sectors
-        patches.append(Wedge((0.,0.), .4, *ang, facecolor='w', lw=2))
+        patches.append(Wedge((0., 0.), .4, *ang, facecolor='w', lw=2))
         # arcs
-        patches.append(Wedge((0.,0.), .4, *ang, width=0.10, facecolor=c, lw=2, alpha=0.5))
+        patches.append(Wedge((0., 0.), .4, *ang, width=0.10, facecolor=c, lw=2, alpha=0.5))
 
     [ax.add_patch(p) for p in patches]
 
@@ -82,28 +85,27 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
     """
 
     for mid, lab in zip(mid_points, labels):
-
-        ax.text(0.35 * np.cos(np.radians(mid)), 0.35 * np.sin(np.radians(mid)), lab, \
-            horizontalalignment='center', verticalalignment='center', fontsize=14, \
-            fontweight='bold', rotation = rot_text(mid))
+        ax.text(0.35 * np.cos(np.radians(mid)), 0.35 * np.sin(np.radians(mid)), lab,
+                horizontalalignment='center', verticalalignment='center', fontsize=14,
+                fontweight='bold', rotation=rot_text(mid))
 
     """
     set the bottom banner and the title
     """
-    r = Rectangle((-0.4,-0.1),0.8,0.1, facecolor='w', lw=2)
+    r = Rectangle((-0.4, -0.1), 0.8, 0.1, facecolor='w', lw=2)
     ax.add_patch(r)
 
-    ax.text(0, -0.05, title, horizontalalignment='center', \
-         verticalalignment='center', fontsize=22, fontweight='bold')
+    ax.text(0, -0.05, title, horizontalalignment='center',
+            verticalalignment='center', fontsize=22, fontweight='bold')
 
     """
     plots the arrow now
     """
 
-    pos = mid_points[abs(cat - N)]
+    pos = mid_points[abs(cat - n)]
 
-    ax.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)), \
-                 width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
+    ax.arrow(0, 0, 0.225 * np.cos(np.radians(pos)), 0.225 * np.sin(np.radians(pos)),
+             width=0.04, head_width=0.09, head_length=0.1, fc='k', ec='k')
 
     ax.add_patch(Circle((0, 0), radius=0.02, facecolor='k'))
     ax.add_patch(Circle((0, 0), radius=0.01, facecolor='w', zorder=11))
@@ -118,7 +120,6 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
     ax.axis('equal')
     plt.tight_layout()
 
-
     # generate the canvas to display the plot
     canvas = FigureCanvas(fig)
 
@@ -129,5 +130,6 @@ def gauge(labels=['LOW','MEDIUM','HIGH','VERY HIGH','EXTREME'], colors='jet_r', 
     win.show()
 
     sys.exit(app.exec_())
+
 
 gauge()
