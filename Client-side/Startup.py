7f1f7e0ed99612117922f605cded6bbc95fd4359
Daniel Alderman <daniel.alderman77@gmail.com>
2016-05-13 13:00:15 +0100

    Startup.py has been amended to follow PEP8

--- Client-side/Startup.py
+++ Client-side/Startup.py
@@ -3,30 +3,31 @@ from RestClient import RestClient
 
 import matplotlib.pyplot as plt
 
-class Startup():
+
+class Startup:
 
     def __init__(self):
         self.name = self
 
-    def MemoryAllocation(self, currentValue):
+    def memory_allocation(self, current_value):
         plt.subplot(221)
-        plt.plot(currentValue)
+        plt.plot(current_value)
         plt.axis([0, 6, 0, 1000])
         plt.ylabel('Memory Allocation')
         plt.xlabel('Time')
         plt.title('Memory Allocation Over Time')
 
-    def Task1(self, currentValue):
+    def task1(self, current_value):
         plt.subplot(222)
-        plt.plot(currentValue)
+        plt.plot(current_value)
         plt.axis([0, 6, 0, 1000])
         plt.ylabel('Memory Allocation')
         plt.xlabel('Time')
         plt.title('Task 1 Memory Usage')
 
-    def Task2(self, currentValue):
+    def task2(self, current_value):
         plt.subplot(223)
-        plt.plot(currentValue)
+        plt.plot(current_value)
         plt.axis()
         plt.ylabel('Memory Allocation')
         plt.xlabel('Time')
@@ -34,21 +35,21 @@ class Startup():
 
     def __call__(self):
 
-        readData = ReadData()
-        memoryData = readData.ParseXML('Client-side/data.xml')
+        read_data = ReadData()
+        memory_data = read_data.parse_xml('Client-side/data.xml')
 
-        restClient = RestClient()
-        restClient.GetRequest()
+        rest_client = RestClient()
+        rest_client.GetRequest()
 
-        totalMemory = int(memoryData[0])
-        task1 = int(memoryData[1])
-        task2 = int(memoryData[2])
+        total_memory = int(memory_data[0])
+        task1 = int(memory_data[1])
+        task2 = int(memory_data[2])
 
         # plot with various axes scales
         plt.figure(1)
 
-        task1Usage = [task1, 400, 350, 500, 400]
-        task2Usage = [task2, 350, 400, 500, 600]
+        task1_usage = [task1, 400, 350, 500, 400]
+        task2_usage = [task2, 350, 400, 500, 600]
 
         plt.ion()
         plt.show()
@@ -56,11 +57,11 @@ class Startup():
         # Number of times the animation refreshes
         for i in range(10):
 
-            memoryAllocation = task1Usage + task2Usage
+            memory_allocation = task1_usage + task2_usage
 
-            self.MemoryAllocation(memoryAllocation)
-            self.Task1(task1Usage)
-            self.Task2(task2Usage)
+            self.memory_allocation(memory_allocation)
+            self.task1(task1_usage)
+            self.task2(task2_usage)
 
             plt.draw()
             plt.pause(0.05)
