f7ce6c442b7c74a2ecf7dd62697c97931b0c1097
Daniel Alderman <daniel.alderman77@gmail.com>
2016-06-21 17:01:10 +0100

    Implemented interface method that counts number of stored XML files

--- Server-side/RestServer.py
+++ Server-side/RestServer.py
@@ -1,10 +1,23 @@
+import glob
+
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__, static_url_path='')
+api = Api(app)
 
 
-@app.route('/')
-def root():
+class Hello(Resource):
+    def get(self):
+        file_count = (len(glob.glob1("static", "*.xml")))
+
+        return {'Number of files': file_count}
+
+api.add_resource(Hello, '/file_count/')
+
+
+@app.route('/data/')
+def get_data():
     return app.send_static_file('data.xml')
 
 if __name__ == '__main__':
