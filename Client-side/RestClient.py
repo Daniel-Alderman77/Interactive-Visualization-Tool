b200f347f5f6d56b51e0f53519e2561a7a4ca3b1
Daniel Alderman <daniel.alderman77@gmail.com>
2016-06-22 16:24:58 +0100

    Client is able to request file by appending filename to url

--- Client-side/RestClient.py
+++ Client-side/RestClient.py
@@ -33,19 +33,48 @@ class RESTClient:
 
         return request
 
-    def get_data(self):
-        print ("")
+    def get_data(self, argument):
+        request = None
+
+        try:
+            session = requests.Session()
+
+            path = 'http://127.0.0.1:5000/data/' + argument
+
+            request = session.get(path, timeout=0.1)
+
+            print(request.status_code)
+
+            print(request.text)
+
+            # Time elapsed between sending the request and arrival of response
+            print(request.elapsed)
+
+        except ReadTimeout:
+            print "Connection has timed out"
+
+        except ConnectionError:
+            print "Failed to establish connection to Server"
+
+        return request
 
     def __call__(self):
         response = self.get_request()
 
         response_dict = json.loads(response.text)
 
+        data = 0
+
         if 'Number of files' in response_dict:
             number_of_files = response_dict['Number of files']
             print number_of_files
 
-        self.get_data()
+            list_of_files = response_dict['List of files']
+            print list_of_files[0]
+
+            data = self.get_data(list_of_files[0])
+
+        print data.text
 
 client = RESTClient()
 client()
