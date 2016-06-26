6eb5bf4e2e64f5a09333448961649273c9e381d4
Daniel Alderman <daniel.alderman77@gmail.com>
2016-06-26 16:07:55 +0100

    Moved Tutorial/Example code to Examples directory

---
+++ Examples/Prediction.py
@@ -0,0 +1,24 @@
+# Based upon
+# http://stackoverflow.com/questions/2745329/how-to-make-scipy-interpolate-give-an-extrapolated-result-beyond-the-input-range
+
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+
+class Prediction:
+
+    def __init__(self):
+        self.name = self
+
+    def predict(self, xi, yi):
+
+        # positions to extrapolate last element in array 1 position
+        x = np.linspace(0, xi[-1], 1)
+
+        # when k = 1, linear extrapolation
+        extrapolate = InterpolatedUnivariateSpline(xi, yi, k=1)
+        y = extrapolate(x)
+
+        results = [x, y]
+
+        return results
