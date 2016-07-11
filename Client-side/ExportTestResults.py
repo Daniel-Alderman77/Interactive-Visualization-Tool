61fa21380850c11b650bb5f9c7e1f7defdad3925
Daniel Alderman <daniel.alderman77@gmail.com>
2016-07-12 00:33:00 +0100

    Created ExportTestResults.py, with TODOs

---
+++ Client-side/ExportTestResults.py
@@ -0,0 +1,12 @@
+# TODO - Write 'Throughput'to file
+# TODO - Write 'Percentage of jobs completed' to file
+# TODO - Write 'Dropped packets' to file
+# TODO - Write 'Faults occurred, by type' to file
+# TODO - Write 'Faults recovered from, by type' to file
+# TODO - Write 'Ping' to file
+
+
+class ExportTestResults:
+
+    def __init__(self):
+        self.name = self
