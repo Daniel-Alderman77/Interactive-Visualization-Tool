e0f146d787946d4b225b8f1127312839faff8c19
Daniel Alderman <daniel.alderman77@gmail.com>
2016-07-11 16:31:39 +0100

    Refactored code from SingularClientTest into RequestData class

--- Client-side/FileHandler.py
+++ Client-side/FileHandler.py
@@ -1,4 +1,53 @@
+import glob
 from lxml import etree
+
+from RESTClient import RESTClient
+
+
+class RequestData:
+
+    def __init__(self):
+        self.name = self
+
+    def get_file_count(self):
+        file_count = (len(glob.glob1("data_store", "*.xml")))
+
+        list_of_files = []
+        if file_count > 0:
+            list_of_files = glob.glob1("data_store", "*.xml")
+
+        return {'Number of files': file_count,
+                'List of files': list_of_files}
+
+    def request_data(self):
+        rest_client = RESTClient()
+
+        read_data = ReadData()
+
+        try:
+            if rest_client.__call__()[0] == True:
+                print "Connection is successful"
+
+                number_of_files = self.get_file_count()["Number of files"]
+
+                if number_of_files:
+                    print "Files have been transferred"
+                    read_data.parse_memory_data('data_store/data.xml')
+                else:
+                    print "Files have not been transferred"
+
+        except:
+            print "Server is unavailable"
+
+            if self.get_file_count()["Number of files"] > 0:
+                print "Files are available for coldstart prediction"
+
+                memory_data = read_data.parse_memory_data('data_store/data.xml')
+
+                total_memory = int(memory_data[0])
+                print total_memory
+            else:
+                print "Files are not available for coldstart prediction"
 
 
 class ReadData:
--- Client-side/SingularClientTest.py
+++ Client-side/SingularClientTest.py
@@ -1,5 +1,4 @@
-import glob
-from FileHandler import ReadData
+from FileHandler import ReadData, RequestData
 from RESTClient import RESTClient
 
 
@@ -8,50 +7,20 @@ class Startup:
     def __init__(self):
         self.name = self
 
-    def get_file_count(self):
-        file_count = (len(glob.glob1("data_store", "*.xml")))
-
-        list_of_files = []
-        if file_count > 0:
-            list_of_files = glob.glob1("data_store", "*.xml")
-
-        return {'Number of files': file_count,
-                'List of files': list_of_files}
-
     def __call__(self):
         rest_client = RESTClient()
 
         read_data = ReadData()
+
+        request_data = RequestData()
 
         # total_number_of_files_available = rest_client.__call__()[1]
         #
         # while self.get_file_count()["Number of files"] <= total_number_of_files_available:
         #     print "Now attempting to retrieve files"
         #
-        try:
-            if rest_client.__call__()[0] == True:
-                print "Connection is successful"
 
-                print self.get_file_count()["Number of files"]
-
-                if self.get_file_count()["Number of files"]:
-                    print "Files have been transferred"
-                    read_data.parse_memory_data('data_store/data.xml')
-                else:
-                    print "Files have not been transferred"
-
-        except:
-            print "Server is unavailable"
-
-            if self.get_file_count()["Number of files"] > 0:
-                print "Files are available for coldstart prediction"
-
-                memory_data = read_data.parse_memory_data('data_store/data.xml')
-
-                total_memory = int(memory_data[0])
-                print total_memory
-            else:
-                print "Files are not available for coldstart prediction"
+        request_data.request_data()
 
 startup = Startup()
 startup()
