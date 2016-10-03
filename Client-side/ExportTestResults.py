708ddf0ba8bc72e5b17c0408d04370ac0d058352
Daniel Alderman <daniel.alderman77@gmail.com>
2016-10-03 15:03:15 +0100

    Copied DictWriter example from docs. Writes CSV file to /test_results

--- Client-side/ExportTestResults.py
+++ Client-side/ExportTestResults.py
@@ -1,3 +1,5 @@
+import csv
+
 # TODO - Write 'Throughput'to file
 # TODO - Write 'Percentage of jobs completed' to file
 # TODO - Write 'Dropped packets' to file
@@ -10,3 +12,17 @@ class ExportTestResults:
 
     def __init__(self):
         self.name = self
+
+    def write_to_file(self):
+        with open('test_results/test.csv', 'wb') as test_file:
+            fieldnames = ['first_name', 'last_name']
+            writer = csv.DictWriter(test_file, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+            writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+            writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
+
+export_test_results = ExportTestResults()
+
+export_test_results.write_to_file()
