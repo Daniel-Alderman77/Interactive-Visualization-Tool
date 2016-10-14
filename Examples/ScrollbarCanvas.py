ab856bbce425c3004132ec58aca982a4cb95884b
Daniel Alderman <daniel.alderman77@gmail.com>
2016-10-14 13:34:32 +0100

    Added ScrollbarCanvas example

---
+++ Examples/ScrollbarCanvas.py
@@ -0,0 +1,14 @@
+from Tkinter import *
+
+root = Tk()
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+my_list = Listbox(root, yscrollcommand=scrollbar.set)
+for line in range(100):
+    my_list.insert(END, "This is line number " + str(line))
+
+my_list.pack(side=LEFT, fill=BOTH)
+scrollbar.config(command=my_list.yview)
+
+mainloop()
