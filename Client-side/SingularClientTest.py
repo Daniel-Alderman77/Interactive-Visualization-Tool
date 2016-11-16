272734fe639f69df1428eac7291c22255bb0465b
Daniel Alderman <daniel.alderman77@gmail.com>
2016-11-16 10:33:54 +0000

    program_loop is now called as a separate thread to prevent blocking

--- Client-side/SingularClientTest.py
+++ Client-side/SingularClientTest.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import threading
+import time
 
 from WebServiceClient import WebServiceClient
 from FileHandler import ResponseDeserialization
@@ -34,34 +35,6 @@ class Startup:
         ping_thread = threading.Thread(target=class_instance.calculate_ping())
         ping_thread.start()
 
-    def initial_loop(self, index):
-
-        # Calls function that continuously calculates ping
-        self.calculate_ping(self.web_service_client)
-
-        self.export_test_results.create_test_file()
-
-        self.export_test_results.write_startup_to_file()
-
-        # Contact server and return number of remote files available
-        number_of_remote_files = self.web_service_client.get_remote_file_count(index)
-
-        print("Number of remote files: %s" % number_of_remote_files)
-
-        # Check file transfer has been successful
-        if self.web_service_client.check_transfer(index):
-            list_of_files = self.web_service_client.get_local_file_count()["List of files"]
-
-            self.export_test_results.write_fetch_to_file(list_of_files[index])
-
-            filename = 'data_store/' + list_of_files[index]
-
-            # Deserialize filename passed as a parameter
-            self.response_deserialization.parse_cpu_data(filename)
-            self.response_deserialization.parse_memory_data(filename)
-            self.response_deserialization.parse_jobs_data(filename)
-            self.response_deserialization.parse_energy_data(filename)
-
     @staticmethod
     def test_run_cleanup():
         # Load filenames in data_store in array
@@ -74,15 +47,53 @@ class Startup:
             except OSError:
                 pass
 
+    def program_loop(self, index):
+
+        # Calls function that continuously calculates ping
+        self.calculate_ping(self.web_service_client)
+
+        self.export_test_results.create_test_file()
+
+        self.export_test_results.write_startup_to_file()
+
+        # Contact server and return number of remote files available
+        number_of_remote_files = self.web_service_client.get_remote_file_count(index)
+
+        number_of_local_files = self.web_service_client.get_local_file_count()['Number of files']
+
+        print("Number of remote files: %s" % number_of_remote_files)
+
+        while number_of_remote_files < number_of_local_files:
+            # Check file transfer has been successful
+            if self.web_service_client.check_transfer(index):
+                list_of_files = self.web_service_client.get_local_file_count()["List of files"]
+
+                self.export_test_results.write_fetch_to_file(list_of_files[index])
+
+                filename = 'data_store/' + list_of_files[index]
+
+                # Deserialize filename passed as a parameter
+                self.response_deserialization.parse_cpu_data(filename)
+                self.response_deserialization.parse_memory_data(filename)
+                self.response_deserialization.parse_jobs_data(filename)
+                self.response_deserialization.parse_energy_data(filename)
+
+                time.sleep(1)
+
+    def main(self, index):
+        # Executes function in background thread
+        main_thread = threading.Thread(target=self.program_loop, args=[index])
+        main_thread.start()
+
     def __call__(self):
         # Start UI
         root = self.user_interface.run()
 
-        # File local datafile index. Increments as each new file is visualized
+        # File local datafile index. Incremets as each new file is visualized
         index = 0
 
         # Start initial loop
-        self.initial_loop(index)
+        self.main(index)
 
         # TODO - Start repeated loop
 
