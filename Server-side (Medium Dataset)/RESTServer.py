4ef34919d26e1fa7c12e4d8c40a409d7542d9d31
Daniel Alderman <daniel.alderman77@gmail.com>
2016-11-28 17:29:39 +0000

    Added data and server code to Server-side (Medium Dataset) and Server-sider (Small Dataset)

---
+++ Server-side (Medium Dataset)/RESTServer.py
@@ -0,0 +1,30 @@
+import glob
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__, static_url_path='')
+api = Api(app)
+
+
+class Server(Resource):
+    def get(self):
+        file_count = (len(glob.glob1("static", "*.xml")))
+
+        list_of_files = []
+        if file_count > 0:
+            list_of_files = glob.glob1("static", "*.xml")
+
+        return {'Number of files': file_count,
+                'List of files': list_of_files}
+
+api.add_resource(Server, '/file_count/')
+
+
+@app.route('/data/<path:path>')
+def get_data(path):
+    return app.send_static_file(path)
+
+# Enabled threading to handle multiple requests
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
